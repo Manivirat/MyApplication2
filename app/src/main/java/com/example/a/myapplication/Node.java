b8d7cbbf3d38fea15e8e0c7e738fe1cc530df3b1
Manivirat <manivirat@gmail.com>
2017-11-14 20:31:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/a/myapplication/Node.java
@@ -0,0 +1,41 @@
+package com.example.a.myapplication;
+
+
+public class Node {
+    public Node nextNode;
+    public String payload;
+
+
+    public Node(String payload)
+    {
+        this.payload = payload;
+        this.nextNode = null;
+    }
+
+    public Node getNextNode()
+    {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode)
+    {
+        this.nextNode = nextNode;
+    }
+
+    public String getPayload()
+    {
+        return payload;
+    }
+
+    public void display()
+    {
+        System.out.print(this.payload);
+        if(this.nextNode != null)
+        {
+            System.out.print("  ");
+            this.nextNode.display();
+        }
+    }
+}
+
+}
